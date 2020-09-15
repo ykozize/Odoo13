f17b59f97f03ce9eceabab4b9b5599485ca9834c
raghadz <rzabibi@altanmya.net>
2020-09-15 12:09:07 +0000

    my commit to Tala

--- school_management/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ school_management/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -6,7 +6,7 @@
         STalaaaaya""",
 
     'description': """
-        Long description of module's purpose
+       Tala's work changed by Raghad 
     """,
 
     'author': "My Company",
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '1.2',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
